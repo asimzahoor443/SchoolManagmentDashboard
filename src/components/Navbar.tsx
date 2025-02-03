063b58c340e8b1c0876caab9d90bf41085e67a04
Asim Zahoor <113241389+asimzahoor443@users.noreply.github.com>
2025-02-03 14:00:23 +0500

    feat: update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,32 @@
+import Image from 'next/image';
 import React from 'react';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="flex items-center justify-between p-4">
+      {/* Search Bar */}
+      <div className="hidden md:flex">
+        <Image src="/search.png" alt="" width={14} height={14} />
+        <input type="text" placeholder="Search..." />
+      </div>
+      {/* Icons And Users */}
+      <div className="flex items-center gap-6">
+        <div className="bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer">
+          <Image src="/message.png" alt="" width={20} height={20} />
+        </div>
+        <div className="bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer">
+          <Image src="/announcement.png" alt="" width={20} height={20} />
+        </div>
+        <div className="flex flex-col">
+          <span className="text-xs leading-3 font-medium">Asim Awan</span>
+          <span className="text-[10px] text-gray-500 text-right">Admin</span>
+        </div>
+        <Image src="/avatar.png" alt="" />
+      </div>
+
+      {/* Search Bar */}
+    </div>
+  );
 };
 
 export default Navbar;
