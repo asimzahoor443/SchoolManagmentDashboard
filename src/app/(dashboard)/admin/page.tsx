52c59a9f1c7eba0abeafc516c95d0372a24cb339
Asim Zahoor <113241389+asimzahoor443@users.noreply.github.com>
2025-02-04 15:23:12 +0500

    feat:updates

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,11 +1,17 @@
+import UserCart from '@/components/UserCart';
+
 const AdminPage = () => {
   return (
     <div className="p-4 flex gap-4 flex-col md:flex-row">
       {/* Left Side */}
-      <div className="w-full lg:w-2/3">{/* User Card */}
-      <div className=""></div>
-
-      </div>
+      <div className="w-full lg:w-2/3">
+        {/* User Card */}
+        <div className="flex gap-4 justify-between">
+          <UserCart type="student" />
+          <UserCart type="teacher" />
+          <UserCart type="parent" />
+          <UserCart type="staff" />
+        </div>
       </div>
       {/* Left Side */}
       <div className="w-full lg:w-1/3">R</div>
--- src/components/UserCart.tsx
+++ src/components/UserCart.tsx
@@ -1,5 +1,16 @@
+import Image from 'next/image';
+
 const UserCart = ({ type }: { type: string }) => {
-  return <div>UserCart</div>;
+  return (
+    <div className="rounded-2xl odd:bg-lamaPurple even:bg-lamaYellow p-4 flex-1">
+      <div className="">
+        <span>2024/25</span>
+        <Image src="/more.png" alt="" width={20} height={20} />
+      </div>
+      <h1>1,234</h1>
+      <h2>{type}</h2>
+    </div>
+  );
 };
 
 export default UserCart;
