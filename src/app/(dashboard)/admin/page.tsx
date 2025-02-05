009ff00a64f85ef4dd3049ae0e0a236be93dc5be
Asim Zahoor <113241389+asimzahoor443@users.noreply.github.com>
2025-02-05 12:19:26 +0500

    docs:CountChart Updates

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,17 +1,29 @@
+import CountChart from '@/components/CountChart';
 import UserCart from '@/components/UserCart';
 
 const AdminPage = () => {
   return (
     <div className="p-4 flex gap-4 flex-col md:flex-row">
       {/* Left Side */}
-      <div className="w-full lg:w-2/3">
+      <div className="w-full lg:w-2/3 flex flex-col gap-8">
         {/* User Card */}
-        <div className="flex gap-4 justify-between">
+        <div className="flex gap-4 justify-between flex-wrap">
           <UserCart type="student" />
           <UserCart type="teacher" />
           <UserCart type="parent" />
           <UserCart type="staff" />
         </div>
+        {/* MIDDLE CHARTS */}
+        <div className="flex gap-4 flex-col lg:flex-row">
+          {/* COUNT CHART */}
+          <div className="w-full lg:w-1/3 h-[450px]">
+            <CountChart />
+          </div>
+          {/* ATTENDENCE CHART */}
+          <div className="w-full lg:w-2/3 h-[450px]"></div>
+        </div>
+        {/* BOTTOM CHARTS */}
+        <div className=""></div>
       </div>
       {/* Left Side */}
       <div className="w-full lg:w-1/3">R</div>
--- src/components/UserCart.tsx
+++ src/components/UserCart.tsx
@@ -3,12 +3,14 @@ import Image from 'next/image';
 const UserCart = ({ type }: { type: string }) => {
   return (
     <div className="rounded-2xl odd:bg-lamaPurple even:bg-lamaYellow p-4 flex-1">
-      <div className="">
-        <span>2024/25</span>
+      <div className="flex justify-between items-center">
+        <span className="text-[10px] bg-white px-2 py-1 rounded-full text-gray-600">
+          2024/25
+        </span>
         <Image src="/more.png" alt="" width={20} height={20} />
       </div>
-      <h1>1,234</h1>
-      <h2>{type}</h2>
+      <h1 className="text-2xl font-semibold my-4">1,234</h1>
+      <h2 className="capitalize text-sm font-medium text-gray-500">{type}s</h2>
     </div>
   );
 };
