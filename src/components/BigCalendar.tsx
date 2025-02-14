0f161ebf900f9feaaa7959d358cc0251c87190f5
Asim Zahoor <113241389+asimzahoor443@users.noreply.github.com>
2025-02-14 11:25:03 +0500

    docs:intializing BigCalendar

---
+++ src/components/BigCalendar.tsx
@@ -0,0 +1,17 @@
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+
+const localizer = momentLocalizer(moment);
+const BigCalendar = (props) => {
+  <div>
+    <Calendar
+      localizer={localizer}
+      events={myEventsList}
+      startAccessor="start"
+      endAccessor="end"
+      style={{ height: 500 }}
+    />
+  </div>;
+};
+
+export default BigCalndar;
