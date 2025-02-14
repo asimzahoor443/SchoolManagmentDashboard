aa33e6de9a304aebd401eed25258bf8d42fffded
Asim Zahoor <113241389+asimzahoor443@users.noreply.github.com>
2025-02-14 18:48:24 +0500

    update

--- src/components/BigCalendar.tsx
+++ src/components/BigCalendar.tsx
@@ -1,12 +1,15 @@
+'use client';
+
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
+import { calendarEvents } from '@/lib/data';
 
 const localizer = momentLocalizer(moment);
-const BigCalendar = (props) => {
+const BigCalendar = () => {
   <div>
     <Calendar
       localizer={localizer}
-      events={myEventsList}
+      events={calendarEvents}
       startAccessor="start"
       endAccessor="end"
       style={{ height: 500 }}
@@ -14,4 +17,4 @@ const BigCalendar = (props) => {
   </div>;
 };
 
-export default BigCalndar;
+export default BigCalendar;
